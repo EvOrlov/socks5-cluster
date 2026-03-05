dec26255e5d2bf10feab84c36c677cc27ac8a0d3
Evgeniy <seaman2774@yandex.ru>
2026-03-05 20:18:43 +0300

    New entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,56 +4,60 @@ CONFIG_PATH="/etc/danted.conf"
 USERS_FILE="/etc/danted/users.txt"
 
 if [ ! -s "$USERS_FILE" ]; then
-    echo "[ERROR] users.txt not found or empty"
+    echo "[ERROR] users.txt not found"
     exit 1
 fi
+
+mkdir -p /var/log
+touch /var/log/danted.log
 
 echo "[INIT] Generating Dante configuration..."
 
 cat <<EOF > $CONFIG_PATH
 logoutput: /var/log/danted.log
+
+internal: 0.0.0.0 port = 1080
 external: eth0
 
-socksmethod: username
-clientmethod: none
+method: username
 
 user.privileged: root
 user.notprivileged: nobody
+user.libwrap: nobody
+
 EOF
 
 while IFS=: read -r login password port
 do
 
-if [ -z "$login" ]; then
-continue
-fi
+[ -z "$login" ] && continue
 
 adduser -D "$login"
 echo "$login:$password" | chpasswd
 
+echo "internal: 0.0.0.0 port = $port" >> $CONFIG_PATH
+
 cat <<EORULE >> $CONFIG_PATH
 
-internal: 0.0.0.0 port = $port
-
 client pass {
-    from: 0.0.0.0/0 to: 0.0.0.0/0
-    port = $port
+    from: 0.0.0.0/0 to: 0.0.0.0/0 port = $port
     log: connect error
+    method: username
 }
 
-socks pass {
-    from: 0.0.0.0/0 to: 0.0.0.0/0
-    port = $port
+pass {
+    from: 0.0.0.0/0 to: 0.0.0.0/0 port = $port
     protocol: tcp udp
+    log: connect error
+    method: username
     username: "$login"
-    log: connect disconnect error
 }
 
 EORULE
 
 done < "$USERS_FILE"
 
-echo "[INFO] Dante config created."
-echo "[INFO] Starting SOCKS server..."
+echo "[INFO] Dante config created"
+echo "[INFO] Starting SOCKS server"
 
-exec /usr/sbin/sockd -f "$CONFIG_PATH"
+exec /usr/sbin/sockd -f $CONFIG_PATH
